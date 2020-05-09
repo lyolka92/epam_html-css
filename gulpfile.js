e55343ed31905d0003a62c656bf21dca2322f649
Olga Serova <lyolka92@gmail.com>
2020-05-09 15:34:33 +0300

    feat: add galp to project

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const del = require('del');
+const scssPath = 'styles/scss/*.scss';
+
+gulp.task('styles', () => {
+    return gulp.src(scssPath)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('styles/css/'));
+});
+
+gulp.task('clean', () => {
+    return del([
+        'styles/css/*.css',
+    ]);
+});
+
+// gulp.task('default', gulp.series(['clean', 'styles']));
+
+gulp.task('watch', () => {
+    gulp.watch(scssPath, (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
