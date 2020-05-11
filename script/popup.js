c98f4f6126faa13ca8ccb5d9aba6b097b377ae51
Olga Serova <lyolka92@gmail.com>
2020-05-11 11:55:10 +0300

    refactor: add some changes to popup.js increase readability of the popup template change overlay and close-btn selectors (now they are searched in the popup, not in whole document) made a separate function for page styles toggling

--- script/popup.js
+++ script/popup.js
@@ -1,25 +1,16 @@
 const subscribeForm = document.getElementById("subscribe_form");
 
-subscribeForm.onsubmit = event => {
+subscribeForm.addEventListener("submit", event => {
     let popupHeader = "Поздравляем!";
     let popupText = "Теперь Вы подписаны на нашу восхитительную рассылку! Ожидайте новостей в ближайшее время.";
-    
+
     let subscribePopup = new Popup();
     subscribePopup.show(popupHeader, popupText);
 
     event.preventDefault();
-}
+});
 
 function Popup() {
-    const closeBtnId = "popup-close-btn";
-    const overlayId = "popup-overlay";
-
-    const template = [`<div class="popup__overlay" id=${overlayId}></div><div class="popup__container"><div class="popup__header"><h2 class="popup__header-text">`,
-        '',
-        `</h2><span class="popup__close-btn" id=${closeBtnId}></span></div><div class="popup__content"><p class="popup__content-text">`,
-        '',
-        '</p></div></div>'];
-
     const header = document.getElementById("header");
     const main = document.getElementById("main");
     const footer = document.getElementById("footer");
@@ -27,40 +18,45 @@ function Popup() {
     let popup;
 
     function buildPopup(title, text) {
-        template[1] = title;
-        template[3] = text;
+        const template = `
+        <div class="popup__overlay"></div>
+        <div class="popup__container">
+            <div class="popup__header">
+                <h2 class="popup__header-text">${title}</h2>
+                <span class="popup__close-btn"></span>
+            </div>
+            <div class="popup__content">
+                <p class="popup__content-text">${text}</p>
+            </div>
+        </div>`;
 
         popup = document.createElement("div");
         popup.className = "popup";
-        popup.innerHTML = template.join("");
+        popup.innerHTML = template;
+    }
+
+    function togglePageStyles() {
+        header.classList.toggle("blur");
+        main.classList.toggle("blur");
+        footer.classList.toggle("blur");
+
+        document.body.classList.toggle("stop-scrolling");
     }
     
     this.show = (title, text) => {
         buildPopup(title, text);
-
         document.body.append(popup);
 
-        document.getElementById(overlayId).addEventListener('click', event => {
-            this.close();
-        })
-        document.getElementById(closeBtnId).addEventListener('click', event => {
-            this.close();
-        })
+        popup.querySelector(".popup__overlay").addEventListener('click', () => this.close());
+        popup.querySelector(".popup__close-btn").addEventListener('click', () => this.close());
 
-        header.classList.add("blur");
-        main.classList.add("blur");
-        footer.classList.add("blur");
-        document.body.classList.add("stop-scrolling");
+        togglePageStyles();
     };
 
     this.close = () => {
         if(popup) {
             document.body.removeChild(popup);
-
-            header.classList.remove("blur");
-            main.classList.remove("blur");
-            footer.classList.remove("blur");
-            document.body.classList.remove("stop-scrolling");
+            togglePageStyles();
         }
     };
 }
