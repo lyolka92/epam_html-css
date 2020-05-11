20803582366cc12b4fb57233ebb531e3115c5191
Olga Serova <lyolka92@gmail.com>
2020-05-11 13:29:39 +0300

    refactor: add some changes to carousel.js change a carousel function's structure increase readability of the carousel function items change selectors (now they are searched not in whole document) fix some variables type to const

--- script/carousel.js
+++ script/carousel.js
@@ -1,90 +1,95 @@
-let items = document.getElementsByClassName("carousel__item");
-let itemsCount = items.length;
-let slide = 0;
-let isMoving = true;
+const projectsSection = document.getElementById("projects");
+let projectsCarouselNode = projectsSection.querySelector(".carousel");
 
-function initCarousel() {
-    setInitialClasses();
-    setEventListeners();
-    isMoving = false;
+let projectsCarousel = new Carousel(projectsCarouselNode);
+projectsCarousel.init();
+
+function Carousel(carouselNode) {
+    const prevBtn = carouselNode.querySelector(".carousel__btn-prev");
+    const nextBtn = carouselNode.querySelector(".carousel__btn-next");
+    const slides = carouselNode.querySelectorAll(".carousel__item")
+    const slidesCount = slides.length;
+
+    let activeSlideIndex = 0;
+    let isMoving = true;
+
+    this.init = function() {
+        setInitialCarouselClasses();
+        setCarouselEventListeners();
+        isMoving = false;
+    }
+
+    function setInitialCarouselClasses() {
+        slides[slidesCount - 1].classList.add("carousel__item__prev");
+        slides[0].classList.add("carousel__item__active");
+        slides[1].classList.add("carousel__item__next");
+    }
+
+    function setCarouselEventListeners() {
+        prevBtn.addEventListener("click", movePrev);
+        nextBtn.addEventListener("click", moveNext);
+    }
+
+    function movePrev() {
+        if (!isMoving) {
+            if (activeSlideIndex === 0) {
+                activeSlideIndex = slidesCount - 1;
+            } else {
+                activeSlideIndex -= 1;
+            }
+            moveTo(activeSlideIndex);
+        }
+    }
+
+    function moveNext() {
+        if (!isMoving) {
+            if (activeSlideIndex === slidesCount - 1) {
+                activeSlideIndex = 0;
+            } else {
+                activeSlideIndex += 1;
+            }
+            moveTo(activeSlideIndex);
+        }
+    }
+
+    function disableMoving() {
+        isMoving = true;
+        setTimeout(() => { isMoving = false }, 500);
+    }
+
+    function moveTo() {
+        if (!isMoving) {
+            disableMoving();
+
+            let newNextSlide = activeSlideIndex + 1;
+            let newPrevSlide = activeSlideIndex - 1;
+
+            if (activeSlideIndex === 0) {
+                newPrevSlide = slidesCount - 1;
+            } else if (activeSlideIndex === slidesCount - 1) {
+                newNextSlide = 0;
+            }
+
+            if (slidesCount > 3) {
+                let oldNextSlide = newNextSlide + 1;
+                let oldPrevSlide = newPrevSlide - 1;
+
+                if (newNextSlide === slidesCount - 1) {
+                    oldNextSlide = 0;
+                }
+
+                if (newPrevSlide === 0) {
+                    oldPrevSlide = slidesCount - 1;
+                }
+
+                slides[oldPrevSlide].className = "carousel__item";
+                slides[oldNextSlide].className = "carousel__item";
+            }
+
+            slides[newPrevSlide].className = "carousel__item carousel__item__prev";
+            slides[activeSlideIndex].className = "carousel__item carousel__item__active";
+            slides[newNextSlide].className = "carousel__item carousel__item__next";
+        }
+    }
 }
 
-initCarousel();
-
-function setInitialClasses() {
-    items[itemsCount - 1].classList.add("carousel__item__prev")
-    items[0].classList.add("carousel__item__active");
-    items[1].classList.add("carousel__item__next");
-};
-
-function setEventListeners() {
-    let prevBtn = document.getElementsByClassName("carousel__btn-prev")[0];
-    let nextBtn = document.getElementsByClassName("carousel__btn-next")[0];
-
-    prevBtn.addEventListener("click", movePrev);
-    nextBtn.addEventListener("click", moveNext);
-};
-
-function movePrev() {
-    if (!isMoving) {
-        if (slide === 0) {
-            slide = itemsCount - 1;
-        } else {
-            slide--;
-        }
-    }
-
-    moveTo(slide);
-};
-
-function moveNext() {
-    if (!isMoving) {
-        if (slide === itemsCount - 1) {
-            slide = 0;
-        } else {
-            slide++;
-        }
-    }
-
-    moveTo(slide);
-};
-
-function disableMoving() {
-    isMoving = true;
-    setTimeout(() => { isMoving = false }, 500);
-};
-
-function moveTo(slide) {
-    if (!isMoving) {
-        disableMoving();
-
-        let newNextSlide = slide + 1;
-        let newPrevSlide = slide - 1;
-
-        if (slide === 0) {
-            newPrevSlide = itemsCount - 1;
-        } else if (slide === itemsCount - 1) {
-            newNextSlide = 0;
-        }
-
-        if (itemsCount > 3) {
-            let oldNextSlide = newNextSlide + 1;
-            let oldPrevSlide = newPrevSlide - 1;
-
-            if (newNextSlide === itemsCount - 1) {
-                oldNextSlide = 0;
-            };
-            
-            if (newPrevSlide === 0) {
-                oldPrevSlide = itemsCount - 1;
-            };
-
-            items[oldPrevSlide].className = "carousel__item";
-            items[oldNextSlide].className = "carousel__item";
-        };
-
-        items[newPrevSlide].className = "carousel__item carousel__item__prev";
-        items[slide].className = "carousel__item carousel__item__active";
-        items[newNextSlide].className = "carousel__item carousel__item__next";
-    }
-}
