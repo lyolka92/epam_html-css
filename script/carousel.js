51db3e8c86ad4219f5c359efbf0867eb0abacc0b
Olga Serova <lyolka92@gmail.com>
2020-05-22 17:39:16 +0300

    refactor: add prototype inheritance to carousel.js

--- script/carousel.js
+++ script/carousel.js
@@ -1,112 +1,110 @@
-const projectsCarouselNode = document.querySelector("#projects .carousel"),
-    projectsCarousel = new Carousel(projectsCarouselNode);
-
-projectsCarousel.init();
+const initialSlideClass = "carousel__item__initial",
+    prevSlideClass = "carousel__item__prev",
+    activeSlideClass = "carousel__item__active",
+    nextSlideClass = "carousel__item__next";
 
 function Carousel(carouselNode) {
     this.prevBtn = carouselNode.querySelector(".carousel__btn-prev");
     this.nextBtn = carouselNode.querySelector(".carousel__btn-next");
     this.slides = carouselNode.querySelectorAll(".carousel__item");
+    this.slidesCount = this.slides.length;
     this.activeSlideIndex = 0;
     this.isMoving = true;
+}
 
-    const initialSlideClass = "carousel__item__initial",
-        prevSlideClass = "carousel__item__prev",
-        activeSlideClass = "carousel__item__active",
-        nextSlideClass = "carousel__item__next",
-        that = this,
-        slidesCount = that.slides.length;
+const CarouselBase = {
+    init: function() {
+        this.setInitialCarouselClasses();
+        this.setCarouselEventListeners();
+        this.isMoving = false;
+    },
 
-    this.init = function() {
-        setInitialCarouselClasses();
-        setCarouselEventListeners();
-        that.isMoving = false;
-    }
+    setInitialCarouselClasses: function() {
+        this.slides[this.slidesCount - 1].classList.add(prevSlideClass);
+        this.slides[0].classList.add(activeSlideClass);
+        this.slides[1].classList.add(nextSlideClass);
+    },
 
-    function setInitialCarouselClasses() {
-        that.slides[slidesCount - 1].classList.add(prevSlideClass);
-        that.slides[0].classList.add(activeSlideClass);
-        that.slides[1].classList.add(nextSlideClass);
-    }
+    setCarouselEventListeners: function() {
+        const carousel = this;
 
-    function setCarouselEventListeners() {
-        that.prevBtn.addEventListener("click", movePrev);
-        that.nextBtn.addEventListener("click", moveNext);
-    }
+        this.nextBtn.addEventListener("click", carousel.moveNext.bind(carousel));
+        this.prevBtn.addEventListener("click", carousel.movePrev.bind(carousel));
+    },
 
-    function movePrev() {
-        if (that.isMoving) return;
+    moveNext: function() {
+        if (this.isMoving) return;
 
-        if (that.activeSlideIndex === 0) {
-            that.activeSlideIndex = slidesCount - 1;
+        if (this.activeSlideIndex === this.slidesCount - 1) {
+            this.activeSlideIndex = 0;
         } else {
-            that.activeSlideIndex -= 1;
+            this.activeSlideIndex += 1;
         }
 
-        moveTo(that.activeSlideIndex);
-    }
+        this.moveTo(this.activeSlideIndex);
+    },
 
-    function moveNext() {
-        if (that.isMoving) return;
+    movePrev: function() {
+        if (this.isMoving) return;
 
-        if (that.activeSlideIndex === slidesCount - 1) {
-            that.activeSlideIndex = 0;
+        if (this.activeSlideIndex === 0) {
+            this.activeSlideIndex = this.slidesCount - 1;
         } else {
-            that.activeSlideIndex += 1;
+            this.activeSlideIndex -= 1;
         }
 
-        moveTo(that.activeSlideIndex);
-    }
+        this.moveTo(this.activeSlideIndex);
+    },
 
-    function disableMoving() {
-        that.isMoving = true;
-        setTimeout(() => { that.isMoving = false }, 500);
-    }
+    moveTo: function(currentSlideIndex) {
+        this.isMoving = true;
+        setTimeout(() => { this.isMoving = false }, 500);
 
-    function getNextIndex(currentSlideIndex) {
-        let nextSlideIndex;
+        const slidesCount = this.slidesCount;
 
-        if (currentSlideIndex === slidesCount - 1) {
-            nextSlideIndex = 0;
-        } else {
-            nextSlideIndex = currentSlideIndex + 1;
+        function getNextIndex(slideIndex) {
+            let nextSlideIndex;
+            if (slideIndex === slidesCount - 1) {
+                nextSlideIndex = 0;
+            } else {
+                nextSlideIndex = slideIndex + 1;
+            }
+            return nextSlideIndex;
         }
 
-        return nextSlideIndex;
-    }
-
-    function getPrevIndex(currentSlideIndex) {
-        let prevSlideIndex;
-
-        if (currentSlideIndex === 0) {
-            prevSlideIndex = slidesCount - 1;
-        } else {
-            prevSlideIndex = currentSlideIndex - 1;
+        function getPrevIndex(slideIndex) {
+            let prevSlideIndex;
+            if (slideIndex === 0) {
+                prevSlideIndex = slidesCount - 1;
+            } else {
+                prevSlideIndex = slideIndex - 1;
+            }
+            return prevSlideIndex;
         }
-
-        return prevSlideIndex;
-    }
-
-    function moveTo(currentSlideIndex) {
-        disableMoving();
 
         const newNextSlideIndex = getNextIndex(currentSlideIndex);
         const newPrevSlideIndex = getPrevIndex(currentSlideIndex);
 
-        if (slidesCount > 3) {
-            const oldNextSlideIndex = getNextIndex(newNextSlideIndex);
-            const oldPrevSlideIndex = getPrevIndex(newPrevSlideIndex);
+        if (this.slidesCount > 3) {
+            const oldNextSlideIndex = getNextIndex(newNextSlideIndex),
+                oldPrevSlideIndex = getPrevIndex(newPrevSlideIndex);
 
-            that.slides[oldPrevSlideIndex].classList.remove(prevSlideClass);
-            that.slides[oldNextSlideIndex].classList.remove(nextSlideClass);
+            this.slides[oldPrevSlideIndex].classList.remove(prevSlideClass);
+            this.slides[oldNextSlideIndex].classList.remove(nextSlideClass);
         }
 
-        that.slides[newPrevSlideIndex].classList.remove(activeSlideClass, initialSlideClass);
-        that.slides[newPrevSlideIndex].classList.add(prevSlideClass);
-        that.slides[that.activeSlideIndex].classList.remove(prevSlideClass, nextSlideClass);
-        that.slides[that.activeSlideIndex].classList.add(activeSlideClass);
-        that.slides[newNextSlideIndex].classList.remove(activeSlideClass, initialSlideClass);
-        that.slides[newNextSlideIndex].classList.add(nextSlideClass);
+        this.slides[newPrevSlideIndex].classList.remove(activeSlideClass, initialSlideClass);
+        this.slides[newPrevSlideIndex].classList.add(prevSlideClass);
+        this.slides[this.activeSlideIndex].classList.remove(prevSlideClass, nextSlideClass);
+        this.slides[this.activeSlideIndex].classList.add(activeSlideClass);
+        this.slides[newNextSlideIndex].classList.remove(activeSlideClass, initialSlideClass);
+        this.slides[newNextSlideIndex].classList.add(nextSlideClass);
     }
 }
 
+Carousel.prototype = CarouselBase;
+
+const projectsCarouselNode = document.querySelector("#projects .carousel"),
+    projectsCarousel = new Carousel(projectsCarouselNode);
+
+projectsCarousel.init();
